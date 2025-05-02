ce9d30b07a97c7d0690a2ea5a45a001b96f2042f
xoaltie <mezencevadaria96@yandex.ru>
2025-05-02 14:13:20 +0700

    feat: добавлен класс Words для выбора слова для игры

---
+++ app/Words.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+final class Words
+{
+    private static ?Words $instance = null;
+    private static array $list;
+
+    private function __construct()
+    {
+        try {
+            self::$list = $this->getFromFile();
+        }
+        catch (\Exception $exception){
+            exit("Файл конфигурации не найден, игра остановлена.\nОбратитесь к администратору.\n");
+        }
+    }
+
+    public static function getInstance(): Words
+    {
+        if (self::$instance === null) {
+            self::$instance = new Words();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * @return array<int, string>
+     * @throws \Exception
+     */
+    private function getFromFile(): array
+    {
+        $path = __DIR__ . '/../resources/words_list.txt';
+
+        if (!file_exists($path)){
+            throw new \Exception("Файл по пути \"{$path}\" не найден!\n");
+        }
+
+        return file($path, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+    }
+
+    public static function getRandomOne(): string
+    {
+        return self::$list[array_rand(self::$list)];
+    }
+}
